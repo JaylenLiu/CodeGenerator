a706d15f8d9e979d20d30e03e2d263c819c72a0a
ljl <1003800834@qq.com>
2019-01-09 16:05:04 +0800

    project init...

---
+++ src/main/java/cn/jaylen/codegenerator/CodegeneratorApplication.java
@@ -0,0 +1,14 @@
+package cn.jaylen.codegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodegeneratorApplication.class, args);
+    }
+
+}
+
